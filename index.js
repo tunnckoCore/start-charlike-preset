d6534788f41d30a12f8b838180c855ee598567e7
tunnckoCore <mameto_100@mail.bg>
2016-12-25 09:09:11 +0200

    chore(init): scaffold

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * start-charlike-preset <https://github.com/tunnckoCore/start-charlike-preset>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function startCharlikePreset () {
+
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * start-charlike-preset <https://github.com/tunnckoCore/start-charlike-preset>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+const test = require('mukla')
+const startCharlikePreset = require('./index')
+
+test('start-charlike-preset', function (done) {
+  startCharlikePreset()
+  done()
+})
